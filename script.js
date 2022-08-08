a7dce114db33aa85c5367726d874f81e3be264bd
Ben Allen <benalleng@gmail.com>
2022-08-08 12:01:59 -0400

    WIP: add button functionality

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-alert('JS is loaded!')
+// IPO pattern = Input -> Process -> Output
+
+// cached element references = varaiables that store references to DOM elements
+const btn = document.querySelector('button');
+const input = document.querySelector('input');
+const list = document.querySelector('ul');
+const body = document.querySelector('body');
+
+// Event listeners
+btn.addEventListener('click', handleButtonPress);
+// body.addEventListener('click', handleButtonPress);
+list.addEventListener('click', handleLiClick);
+
+
+// Functions
+function handleButtonPress(event) {
+    while (input.value != '') {
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    input.value = '';
+    // how can i set to an empty string
+    list.appendChild(li);
+    }
+    input.focus();
+}
+
+function handleLiClick() {
+    // only allow for li tags to trigger events
+    if (event.target.tagName === 'LI') {
+
+        if (event.target.style.textDecoration === 'none') {
+            event.target.style.textDecoration = 'line-through';
+        } else {
+            event.target.style.textDecoration = 'none';
+        }
+        // console.log('ul tag lorem');
+    } 
+}
